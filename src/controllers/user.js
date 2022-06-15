69bd7867b94b71b770966cb93fbd5abd5b7f93ec
Jedeye <jody.septiawan5@gmail.com>
2022-06-15 09:12:00 +0700

    update

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,5 +1,22 @@
 // import model here
+const { user } = require('../../models')
 
 exports.addUsers = async (req, res) => {
-    // code here
+   try {
+
+    const data = await user.create(req.body)
+
+    res.send({
+        status: 'success',
+        message: 'Add user finished',
+        data
+    })
+    
+   } catch (error) {
+    console.log(error)
+    res.send({
+        status: 'failed',
+        message: 'Server Error'
+    })
+   }
 }
